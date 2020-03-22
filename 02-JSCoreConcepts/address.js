d62fc423b95cd2a5a79a6b497cd5ed8a1a60cdb0
Zielinski, Radoslaw <radoslaw.zielinski@accenture.com>
2020-03-22 22:36:00 +0100

    Address task solution added.

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -6,10 +6,22 @@ city = 'Kraków';
 zip = '30-781';
 country = 'Poland';
 
-
 /* 
 dr Mateusz Kubicki
 ul. Długa 15
 30-781 Kraków
 POLAND
 */
+let address = title + ' ' + name + ' ' + surname + '\n' 
+            + 'ul. ' + street + '\n'
+            + zip + ' ' + city + '\n'
+            + country.toUpperCase();
+
+console.log(address);
+
+address = `\n${title} ${name} ${surname}
+ul. ${street}
+${zip} ${city}
+${country.toUpperCase()}`
+
+console.log(address);
